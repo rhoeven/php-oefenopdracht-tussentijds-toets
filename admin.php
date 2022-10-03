cab25c658d8718db9c3db8395288eb4c479571d8
rhoeven <302073783@student.rocmondriaan.nl>
2022-10-03 11:50:57 +0200

    opdracht2 klaar

---
+++ admin.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>admin</title>
+    </head>
+    <body>
+    <h1>ADMIN</h1>
+    <h3>je bent op de admin pagina</h3>
+    </body>
+</html>
--- opdracht2.php
+++ opdracht2.php
@@ -1,1 +1,40 @@
 <?php
+    if (isset($_POST['submit'])) {
+        $user = filter_input(INPUT_POST, 'user', FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if (!empty($user) && !empty($password)) {
+            if ($user == 'test@gmail.com' && $password == '1234') {
+                header('location:admin.php');
+            } else {
+                $melding = 'Uw gebruikersnaam en/of wachtwoord is niet bekend bij ons';
+            }
+        } else {
+            $melding = "vul alle velden in";
+        }
+    } else {
+        $melding = '';
+    }
+?>
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>opdracht 2</title>
+    </head>
+    <body>
+        <form method="post">
+            <label for="user">Gebruikersnaam:</label>
+            <input type="text" id="user" name="user" value=""/>
+            <br>
+            <label for="password">Wachtwoord:</label>
+            <input type="password" id="password" name="password" value=""/>
+            <br>
+            <br>
+            <input type="submit" name="submit">
+        </form>
+    </body>
+</html>
+
+<?php
+    echo $melding;
+?>
